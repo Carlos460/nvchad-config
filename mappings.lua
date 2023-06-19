80630d56b60bc43f4f8ed4206d094b7efec4e799
Carlos Flores <carlosfloresalex@gmail.com>
2023-06-19 06:29:31 -0400

    created custom config to nvchad

---
+++ mappings.lua
@@ -0,0 +1,26 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>tt"] = {
+      function ()
+        require('base46').toggle_transparency()
+      end,
+     },
+  },
+}
+
+M.disabled = {
+  n = {
+    ["<C-h>"] = "",
+    ["<C-j>"] = "",
+    ["<C-k>"] = "",
+    ["<C-l>"] = "",
+  }
+}
+
+-- more keybinds!
+
+return M
